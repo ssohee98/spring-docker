2e3c73aac24aae02aeb52237827a895e76e07c78
ssohee <jsohee98@naver.com>
2023-03-30 10:15:11 +0900

    Setting

---
+++ src/main/java/com/example/springdocker/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.example.springdocker.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String hello() {
+        return "Hello Docker2";
+    }
+}
